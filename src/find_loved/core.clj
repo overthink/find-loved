1a2b23e817437396d50a300a8abbb235259ae931
Mark Feeney <mark.feeney@gmail.com>
2012-03-14 23:21:23 -0400

    Added scanning filesystem and pulling data from music file tags.  Unfortunately jaudiotagger seems very slow.  It also uses JUL which is a huge pain.  Need to investigate slowness or find another tag reader.

--- src/find_loved/core.clj
+++ src/find_loved/core.clj
@@ -6,17 +6,13 @@
             [clojure.xml :as xml]
             [clojure.zip :as zip]
             [clojure.java.io :as io])
+  (:import [org.jaudiotagger.audio AudioFileIO AudioFileFilter]
+           [org.jaudiotagger.tag FieldKey])
   (:gen-class))
 
 ; Some constants
 (def LIMIT 50)
 (def LOVED_URL "http://ws.audioscrobbler.com/2.0/?method=user.getlovedtracks&user=%s&api_key=%s&page=%d&limit=%d")
-
-; A track returned from last.fm's API
-(defrecord LovedTrack [artist-name track-name date-added])
-
-; A track found in the local file system
-(defrecord FsTrack [artist-name track-name path])
 
 (defn get-api-key
   "Returns the last.fm API key found in ~/.lastfm_api_key, or nil if that can't
@@ -27,6 +23,12 @@
     (when (.exists f)
       (.trim (slurp f)))))
 
+; A track returned from last.fm's API
+(defrecord LovedTrack [artist-name track-name date-added])
+
+; A track found in the local file system
+(defrecord FsTrack [artist-name track-name album-name path])
+
 (defn mk-loved-track
   "Turn the given Clojure XML zipper into a LovedTrack record."
   [z]
@@ -34,6 +36,20 @@
       (xml1-> z :artist :name text)
       (xml1-> z :name text)
       (xml1-> z :date (attr :uts))))
+
+; We only need one of these filters, so make it a 'global' in the namespace.
+(def audio-file-filter (AudioFileFilter. false))
+
+(defn mk-fs-track
+  "Make a track record for a music file on the local filesystem.  Returns a
+  FsTrack if possible, or nil if we couldn't read meta-data from the file."
+  [^java.io.File file]
+  (when (.accept audio-file-filter file)
+    (let [tag (-> (AudioFileIO/read file) (.getTag))
+          artist (.getFirst tag FieldKey/ARTIST)
+          track (.getFirst tag FieldKey/TITLE)
+          album (.getFirst tag FieldKey/ALBUM)]
+      (FsTrack. artist track album (.getAbsolutePath file)))))
 
 (defn str-to-xml 
   "Parse given string into Clojure's tag/attrs/content format."
@@ -56,7 +72,7 @@
   that can be coerced into a File."
   [f]
   (with-open [r (java.io.PushbackReader. (io/reader f))]
-    (binding [*read-eval* false]  ; blow up if reader macro '#=' shows up in file
+    (binding [*read-eval* false]  ; don't allow eval reader macro '#='
       (read r))))
 
 (defn get-tracks
@@ -88,12 +104,18 @@
                                 ["--debug" "Output a lot of junk" :flag true])
         api-key (or (:api-key opts) (get-api-key))
         [user & roots] args
-        tracks (get-tracks-cached user api-key)]
+        fs-tracks (->> roots 
+                    (map io/as-file)
+                    (mapcat file-seq)
+                    (map mk-fs-track))
+        loved-tracks (get-tracks-cached user api-key)]
     (when (:help opts)
       (println "find-loved LASTFM-USERNAME FS-ROOT0 [RS-ROOT1 [..]]")
       (println banner)
       (System/exit 1))
-    (doseq [t tracks]
+    (doseq [t loved-tracks]
       (println (format "%s - %s" (:artist-name t) (:track-name t))))
-    (println (format "\nTotal: %d tracks" (count tracks)))))
+    (doseq [t fs-tracks]
+      (prn t))
+    (println (format "\nTotal: %d tracks" (count loved-tracks)))))
 
